f47fc665792b05a7f96429928c9338e446945378
Anh <addao823@gmail.com>
2021-12-21 15:10:42 -0800

    Initial set up

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,12 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
-  logging: false
-});
+const db = new Sequelize(
+  process.env.DATABASE_URL ||
+    `postgres://${process.env.POSTGRES_USERNAME}:${process.env.POSTGRES_PASSWORD}@localhost:5432/messenger`,
+  {
+    logging: false,
+    dialect: "postgres",
+  }
+);
 
 module.exports = db;
